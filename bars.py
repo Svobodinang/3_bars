28e9bce4edac6d5ea95000e594ee3f36dcf48e77
svobodinang <svobodinang@mail.ru>
2018-04-17 14:46:46 +0300

    pep8

--- bars.py
+++ bars.py
@@ -4,13 +4,14 @@ import math
 
 def load_data(filepath):
     with open(filepath, encoding="utf-8", newline="") as f:
-    	return json.load(f)
+        return json.load(f)
 
 
 def get_biggest_bar(data_bars):
     bars = data_bars["features"]
 
-    max_bar = max(bars, key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
+    max_bar = max(
+        bars, key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
 
     return max_bar
 
@@ -18,7 +19,8 @@ def get_biggest_bar(data_bars):
 def get_smallest_bar(data_bars):
     bars = data_bars["features"]
 
-    min_bar = min(bars, key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
+    min_bar = min(
+        bars, key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
 
     return min_bar
 
@@ -30,16 +32,20 @@ def get_bar_coordinate():
         print("Это не число!")
     return coordinate
 
-def formula(x, y, z):
-    return math.sqrt(pow(x, 2) + pow(y, 2)) - z
+
+def formula(x, y, compared_value):
+    return math.sqrt(pow(x, 2) + pow(y, 2)) - compared_value
+
 
 def get_closest_bar(data_bars, longitude, latitude):
     our_bar_distance_to_zero = math.sqrt(pow(longitude, 2) + pow(latitude, 2))
 
     bars = data_bars["features"]
 
-    closest_bar = min(bars, key=lambda x: 
-        formula(x["geometry"]["coordinates"][0], x["geometry"]["coordinates"][1], our_bar_distance_to_zero))
+    closest_bar = min(
+        bars, key=lambda x: formula(
+            x["geometry"]["coordinates"][0],
+            x["geometry"]["coordinates"][1], our_bar_distance_to_zero))
 
     return closest_bar
 
