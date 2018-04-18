2b1c9514c903911ebfdded153510fdc1bc8537bd
svobodinang <svobodinang@mail.ru>
2018-04-19 00:57:07 +0300

    add input from command line and check this input

--- README.md
+++ README.md
@@ -4,65 +4,29 @@
 
 Уже скачанные данные можно найти по ссылке: https://devman.org/media/filer_public/95/74/957441dc-78df-4c99-83b2-e93dfd13c2fa/bars.json 
 
-Скачанный файл стоит поместить в корневую папку, и указать просто его название.
+Скачанный файл стоит поместить в корневую папку, и указать его название.
 
 # Как запустить
 
-Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5 и выше
 
 Запуск на Linux:
 
 ```bash
 
-$ python bars.py # possibly requires call of python3 executive instead of just python
+$ python bars.py <"название фала  с данными".json># possibly requires call of python3 executive instead of just python
 
 Пример ответа скрипта: 
 
-введите путь к файлу: bars.json
+C:\Users\svobodinang\Desktop\Python\3_bars>python bars.py a.json
 САМЫЙ БОЛЬШОЙ БАР:
-{
-    "geometry": {
-        "coordinates": [
-            37.638228501070095,
-            55.70111462948684
-        ],
-        "type": "Point"
-    },
-    "properties": {
-        "Attributes": {
-            "Address": "Автозаводская улица, дом 23, строение 1",
-            "AdmArea": "Южный административный округ",
-            "District": "Даниловский район",
-            "IsNetObject": "нет",
-            "Name": "Спорт бар «Красная машина»",
-            "OperatingCompany": null,
-            "PublicPhone": [
-                {
-                    "PublicPhone": "(905) 795-15-84"
-                }
-            ],
-            "SeatsCount": 450,
-            "SocialPrivileges": "нет",
-            "global_id": 169375059
-        },
-        "DatasetId": 1796,
-        "ReleaseNumber": 2,
-        "RowId": "fbe6c340-4707-4d74-b7ca-2b84a23bf3a8",
-        "VersionNumber": 2
-    },
-    "type": "Feature"
-}
-
-ЕСЛИ ПРИ ПРОСЬБЕ ПРОГРАММЫ ВВЕСТИ ВАШИ КООРДИНАТЫ ВЫ ВВЕДЕТЕ НЕ ЧИСЛО, СРАБОТАЕТ ИСКЛЮЧЕНИЕ:
-
-введите координатy: j
-ЭТО ИСКЛЮЧЕНИЕ: ВЫ ВВЕЛИ НЕ ЧИСЛО!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-Traceback (most recent call last):
-  File "bars.py", line 67, in <module>
-    get_closest_bar(data, get_bar_coordinate(), get_bar_coordinate())
-  File "bars.py", line 37, in get_bar_coordinate
-    return coordinate
-UnboundLocalError: local variable 'coordinate' referenced before assignment
+"Спорт бар «Красная машина»"
+САМЫЙ МАЛЕНЬКИЙ БАР:
+"БАР. СОКИ"
+БЛИЖАЙШИЙ БАР:
+введите координатy: 77
+введите координатy: 55
+"Staropramen"
 
 ```
 
--- bars.py
+++ bars.py
@@ -1,49 +1,51 @@
 import json
 import math
+import sys
+import os
 
 
 def load_data(filepath):
-    with open(filepath, encoding="utf-8", newline="") as bars:
-        return json.load(bars)
-
-
-def output_json(data_bars):
-    print(json.dumps(data_bars, sort_keys=True, ensure_ascii=False, indent=4))
-
-
-def get_biggest_bar(data_bars):
+    with open(filepath, encoding="utf-8") as data_bar:
+        data_bars = json.load(data_bar)
     bars = data_bars["features"]
+    return bars
 
+
+def output_json(bars):
+    print(json.dumps(
+                     bars["properties"]["Attributes"]["Name"],
+                     sort_keys=True, ensure_ascii=False, indent=4))
+
+
+def get_biggest_bar(bars):
     max_bar = max(
         bars, key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
-    output_json(max_bar)
+    return max_bar
 
 
-def get_smallest_bar(data_bars):
-    bars = data_bars["features"]
+def get_smallest_bar(bars):
 
     min_bar = min(
         bars, key=lambda x: x["properties"]["Attributes"]["SeatsCount"])
-
-    output_json(min_bar)
+    return min_bar
 
 
 def get_user_coordinate():
     try:
         coordinate = float(input("введите координатy: "))
     except ValueError:
-        print("ЭТО ИСКЛЮЧЕНИЕ: ВЫ ВВЕЛИ НЕ ЧИСЛО!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    return coordinate
+        print("ВЫ ВВЕЛИ НЕ ЧИСЛО!!! Повторите попытку ввода координат")
+        return get_user_coordinate()
+    else:
+        return coordinate
 
 
 def search_distance_from_user_to_bar(x, y, compared_value):
     return math.sqrt(pow(x, 2) + pow(y, 2)) - compared_value
 
 
-def get_closest_bar(data_bars, longitude, latitude):
+def get_closest_bar(bars, longitude, latitude):
     our_distance_to_zero = math.sqrt(pow(longitude, 2) + pow(latitude, 2))
-
-    bars = data_bars["features"]
 
     closest_bar = min(
         bars, key=lambda x: search_distance_from_user_to_bar(
@@ -51,16 +53,32 @@ def get_closest_bar(data_bars, longitude, latitude):
             x["geometry"]["coordinates"][1],
             our_distance_to_zero))
 
-    output_json(closest_bar)
+    return closest_bar
+
+
+def input_json_path():
+    file_json = sys.argv[1]
+
+    dirname, exname = os.path.splitext(file_json)
+
+    if os.path.isfile(file_json):
+        if exname == ".json":
+            return file_json
+        else:
+            print("Данный файл имеет расширение не .json")
+            exit(0)
+    else:
+        print("Такого файла не существует")
+        exit(0)
 
 
 if __name__ == "__main__":
-    filepath = input("введите путь к файлу: ")
-    filepath
-    data_bars = load_data(filepath)
+    data_bars = load_data(input_json_path())
     print("САМЫЙ БОЛЬШОЙ БАР:")
-    get_biggest_bar(data_bars)
+    output_json(get_biggest_bar(data_bars))
     print("САМЫЙ МАЛЕНЬКИЙ БАР: ")
-    get_smallest_bar(data_bars)
+    output_json(get_smallest_bar(data_bars))
     print("БЛИЖАЙШИЙ БАР:")
-    get_closest_bar(data_bars, get_user_coordinate(), get_user_coordinate())
+    output_json(get_closest_bar(data_bars,
+                                get_user_coordinate(),
+                                get_user_coordinate()))
